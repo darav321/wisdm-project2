4af9c05bbe849b2f5ec0b662381be780eb927e2c
varad <varadkale20@gmail.com>
2025-12-04 02:04:18 +0530

    improved search functionality

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let quote_array = [quote_1, quote_2, quote_3, quote_4]
 let count = 0;
 
 document.getElementById("left-arrow").addEventListener("click", () => {
-    count = (count + 1) %4;
+    count = (count + 1) % 4;
     let prev_quote = quote_array[(count - 1 + 4) % 4];
     let current_quote = quote_array[count];
 
@@ -18,7 +18,7 @@ document.getElementById("left-arrow").addEventListener("click", () => {
 
 document.getElementById("right-arrow").addEventListener("click", () => {
     prev_count = count;
-    count = (count+3) % 4;
+    count = (count + 3) % 4;
     let prev_quote = quote_array[prev_count];
     let current_quote = quote_array[count];
 
@@ -38,30 +38,26 @@ const findClass = document.getElementById("rounded-nav-link")
 const searchBar = document.getElementById("nav-search");
 
 searchBtn.addEventListener("click", () => {
-    if(searchBar.style.display == "block")
-    {
+    if (searchBar.style.display == "block") {
         searchBar.style.display = "none"
     }
-    else
-    {
+    else {
         searchBar.style.display = "block"
     }
 });
 
 findClass.addEventListener("click", () => {
-    if(searchBar.style.display == "block")
-    {
+    if (searchBar.style.display == "block") {
         searchBar.style.display = "none"
     }
-    else
-    {
+    else {
         searchBar.style.display = "block"
     }
 });
 
 const signinBtn = document.getElementById("signin-btn")
 
-signinBtn.addEventListener("click", ()=> {
+signinBtn.addEventListener("click", () => {
     document.querySelector(".pop-up").style.display = "flex";
 })
 
@@ -74,7 +70,7 @@ closeBtn.addEventListener("click", () => {
 function searchWord() {
     const keyword = document.getElementById("nav-search-input").value.trim();
 
-    if(!keyword) return;
+    if (!keyword) return;
 
     document.querySelectorAll(".highlight").forEach(el => {
         el.classList.remove("highlight")
@@ -85,7 +81,7 @@ function searchWord() {
     const regex = new RegExp(`\\b(${keyword})\\b`, "gi");
     const match = bodyText.match(regex);
 
-    if(!match) {
+    if (!match) {
         alert("No matches")
         return;
     }
@@ -95,29 +91,41 @@ function searchWord() {
 
     const first = document.querySelector(".highlight")
 
-    if(first) {
-        first.scrollIntoView({behavior: "smooth", block: "center"});
+    if (first) {
+        first.scrollIntoView({ behavior: "smooth", block: "center" });
     }
 
     document.querySelector(".occurences").style.display = "flex";
+
+    document.getElementById("close-occ").addEventListener("click", () => {
+        clearHighlights();
+        document.querySelector(".occurences").style.display = "none";
+    })
+
+    function clearHighlights() {
+        document.querySelectorAll(".highlight").forEach(el => {
+            el.replaceWith(document.createTextNode(el.textContent));
+        });
+    }
 
 }
 
 const jumpBtn = document.getElementById("jump_to_top")
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 400) {
+    if (window.scrollY > 400) {
         jumpBtn.style.display = "block"
     }
-    else 
-    {
+    else {
         jumpBtn.style.display = "none"
     }
 })
 
 jumpBtn.addEventListener("click", () => {
-    window.scrollTo({top: 0, behavior: "smooth"})
+    window.scrollTo({ top: 0, behavior: "smooth" })
 })
+
+
 
 
 
